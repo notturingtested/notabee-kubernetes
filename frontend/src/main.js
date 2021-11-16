7d43c8483916f2381e5690467dbff012f0d384ab
Braydn Tanner <btanner@simplenexus.com>
2021-11-15 17:07:48 -0700

    kubernetes start

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,7 @@ import vuetify from './plugins/vuetify'
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'http://notabee-backend.default.svc.cluster.local:3001',
 })
 
 Vue.config.productionTip = false
